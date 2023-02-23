283f0a7f5e8bc7ddc505a8d9066f1afe28bc4b85
kall28 <kalpeshsalvi@gmail.com>
2023-02-24 00:14:45 +0530

    slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,4 +2,5 @@ $(function () {
     $("#header").load("header.html");
     $("#header_inside").load("header_inside.html");
     $("#footer").load("footer.html");
+    $("#modalPage").load("modal-page.html");
 })
