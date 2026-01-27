675e145aa79b3482f017b703333414eb07fd29c9
ksalvi <kalpesh.salvi@dxc.com>
2026-01-27 17:29:36 +0530

    promotion first

---
+++ promotion_first/assets/js/custom.js
@@ -0,0 +1,6 @@
+$(function () {
+    $("#header").load("header.html");
+    $("#header_inside").load("header_inside.html");
+    $("#footer").load("footer.html");
+    $("#modalPage").load("modal-page.html");
+})
